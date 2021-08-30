e0cc691c1e82a44be4565f7da2fddd4e932b0b55
Pharshid Imanipour <farshid.imanipour@gmail.com>
2021-08-30 23:08:13 +0430

    Move db folder

--- db/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS shortlink (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  token VARCHAR(32) NOT NULL UNIQUE,
-  target_url VARCHAR(500) NOT NULL,
-  created_at DATE NOT NULL,
-  visits INT NOT NULL
-);
